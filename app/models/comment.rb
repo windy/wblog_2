eb0d6239a08e4def2487699740bcf7d3ed395a0f
Clacky <develop@clacky.ai>
2025-02-13 03:51:31 -0500

    feat: Implement comment system with sensitive word filtering

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,8 @@ class Comment < ApplicationRecord
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :content, presence: true, length: { minimum: 4 }
 
+  before_save :filter_sensitive_words
+
   def reply_emails
     Comment.where(post_id: self.post_id).collect(&:email).uniq - [ self.email ] - Subscribe.unsubscribe_list - [ ENV['ADMIN_USER'] ]
   end
@@ -19,5 +21,12 @@ class Comment < ApplicationRecord
       Rails.logger.info 'comment created, reply worker start'
       NewReplyPostWorker.perform_async(self.id.to_s)
     end
+  end
+
+  private
+
+  def filter_sensitive_words
+    return if content.blank?
+    self.content = SensitiveWordService.new.filter(content)
   end
 end
