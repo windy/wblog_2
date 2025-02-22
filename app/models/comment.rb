17b8dd8885c65c23fa02fb663d1eaf9b39d2dab7
Clacky <develop@clacky.ai>
2025-02-22 08:51:43 -0500

    Add sensitive words filtering to comment system

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,7 @@ class Comment < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :content, presence: true, length: { minimum: 4 }
+  validate :validate_content_sensitivity
 
   def reply_emails
     Comment.where(post_id: self.post_id).collect(&:email).uniq - [ self.email ] - Subscribe.unsubscribe_list - [ ENV['ADMIN_USER'] ]
@@ -18,6 +19,18 @@ class Comment < ApplicationRecord
     if ENV['MAIL_SERVER'].present?
       Rails.logger.info 'comment created, reply worker start'
       NewReplyPostWorker.perform_async(self.id.to_s)
+    end
+  end
+
+  private
+
+  def validate_content_sensitivity
+    sensitive_words = YAML.load_file(Rails.root.join('config', 'ss.yml'))['sensitive_words']
+    sensitive_words.each do |word|
+      if content&.include?(word)
+        errors.add(:content, :sensitive_word, word: word)
+        break
+      end
     end
   end
 end
